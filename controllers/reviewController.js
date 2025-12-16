11a6410ab8ab52184a4da6eb9d6633d9232e8dd7
Mohamed <ramy.adam33@gmail.com>
2025-12-16 16:21:49 +0400

    feat: implement update and delete review functionlity

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -40,6 +40,65 @@ exports.createReview = async (req, res, next) => {
   }
 };
 
-exports.updateReview = (req, res, next) => {};
+exports.updateReview = async (req, res, next) => {
+  try {
+    // TODO Crucially, you add a second check to ensure the user making the request owns the review before updating it:
+    /**
+ * const reviewId = req.params.id;
+const userIdFromToken = req.user.id; // Added by middleware
 
-exports.deleteReview = (req, res, next) => {};
+const updatedReview = await ReviewModel.findOneAndUpdate(
+    { _id: reviewId, userId: userIdFromToken }, // <-- Check ownership here
+    req.body,
+    { new: true, runValidators: true }
+);
+
+if (!updatedReview) {
+    // If null, either the review doesn't exist OR the current user doesn't own it
+    return res.status(404).json({ message: "Review not found or user not authorized" });
+}
+ */
+
+    const updatedReview = await Review.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    if (!updatedReview) throw new Error('No review found with this id');
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        review: updatedReview,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'failed to updated the review',
+      err,
+    });
+  }
+};
+
+exports.deleteReview = async (req, res, next) => {
+  try {
+    const deletedReview = await Review.findByIdAndDelete(req.params.id, {
+      runValidators: true,
+    });
+
+    if (!deletedReview) throw new Error('Could not delte the review');
+
+    res.status(200).json({
+      status: 'success',
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'failed to updated the review',
+      err,
+    });
+  }
+};
