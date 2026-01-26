186e77e63d127b429512b3212229d46565107316
Mohamed <ramy.adam33@gmail.com>
2026-01-26 20:59:14 +0400

    chore(database): handle database connection failure and cleanup comments

--- server.js
+++ server.js
@@ -8,11 +8,15 @@ const port = process.env.PORT || 3000;
 
 const DB = process.env.DB.replaceAll('<PASSWORD>', process.env.PASSWORD);
 
-mongoose.connect(DB).then((con) => {
-  console.log('Connection established ✅');
-  console.log('Connected to database:', mongoose.connection.name); // ADD THIS LINE
-  console.log('Host:', mongoose.connection.host); // ADD THIS LINE
-});
+mongoose
+  .connect(DB)
+  .then((con) => {
+    console.log('Connection established ✅');
+  })
+  .catch((err) => {
+    console.log('Database connection failed', err.message);
+    process.exit(1);
+  });
 
 app.listen(port, () => {
   console.log(`App is running on port number ${port}`);
